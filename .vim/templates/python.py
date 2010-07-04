782810390b8f5c5e1c506e7045d0e789809f3298
Chris Lasher <chris.lasher@gmail.com>
2010-07-04 12:02:31 -0400

    Updating the python template to include and import of sys, and the __main__ idiom.

--- .vim/templates/python.py
+++ .vim/templates/python.py
@@ -7,3 +7,14 @@
 
 __author__ = 'Chris Lasher'
 __email__ = 'chris DOT lasher <AT> gmail DOT com'
+
+
+import sys
+
+
+def main(argv):
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
