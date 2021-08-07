464fd45102c91a3141ab1634cf54cc0ca875cfd7
Chris Lasher <chris.lasher@gmail.com>
2021-08-06 21:38:53 -0700

    Fix telescope mappings to be global.

--- private_dot_config/nvim/lua/telescope-config.lua
+++ private_dot_config/nvim/lua/telescope-config.lua
@@ -16,18 +16,18 @@ require("telescope").setup {
   }
 }
 
-local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+local function set_keymap(...) vim.api.nvim_set_keymap(bufnr, ...) end
 local opts = { noremap=true, silent=true }
 
 -- General keymaps
-buf_set_keymap('n', '<leader>tb', '<cmd>Telescope buffers<CR>', opts)
-buf_set_keymap('n', '<leader>tf', '<cmd>Telescope find_files<CR>', opts)
-buf_set_keymap('n', '<leader>tg', '<cmd>Telescope live_grep<CR>', opts)
-buf_set_keymap('n', '<leader>th', '<cmd>Telescope help_tags<CR>', opts)
+set_keymap('n', '<leader>tb', '<cmd>Telescope buffers<CR>', opts)
+set_keymap('n', '<leader>tf', '<cmd>Telescope find_files<CR>', opts)
+set_keymap('n', '<leader>tg', '<cmd>Telescope live_grep<CR>', opts)
+set_keymap('n', '<leader>th', '<cmd>Telescope help_tags<CR>', opts)
 
 -- LSP keymaps (see lsp-config for others)
-buf_set_keymap('n', 'gd', '<Cmd>Telescope lsp_definitions<CR>', opts)
-buf_set_keymap('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
-buf_set_keymap('n', '<leader>ca', '<cmd>Telescope lsp_code_actions theme=get_cursor<CR>', opts)
-buf_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
-buf_set_keymap('n', '<leader>e', '<cmd>Telescope lsp_document_diagnostics<CR>', opts)
+set_keymap('n', 'gd', '<Cmd>Telescope lsp_definitions<CR>', opts)
+set_keymap('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
+set_keymap('n', '<leader>ca', '<cmd>Telescope lsp_code_actions theme=get_cursor<CR>', opts)
+set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
+set_keymap('n', '<leader>e', '<cmd>Telescope lsp_document_diagnostics<CR>', opts)
