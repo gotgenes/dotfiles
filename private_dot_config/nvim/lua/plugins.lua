229ec3d4403a5e86ada8b3924154092252702a7a
Chris Lasher <chris.lasher@gmail.com>
2021-08-27 13:48:56 -0700

    Switch to nvim-cmp; nvim-compe is deprecated.

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -5,15 +5,33 @@ return require('packer').startup(function(use)
   use 'Shougo/neoyank.vim'
 
   use {
-    'hrsh7th/nvim-compe',
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-path'
+    },
     config = function()
-      require('compe').setup {
-        source = {
-          path = true,
-          buffer = true,
-          calc = true,
-          nvim_lsp = true,
-          nvim_lua = true,
+      local cmp = require('cmp')
+      cmp.setup {
+        mapping = {
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          })
+        },
+        sources = {
+          { name = 'buffer' },
+          { name = 'nvim_lsp' },
+          { name = 'nvim_lua' },
+          { name = 'path' },
         },
       }
     end
