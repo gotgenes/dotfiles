43295a874e5c4b0c422a1e13eb55e61d6f673b4c
Chris Lasher <chris.lasher@gmail.com>
2021-09-03 08:37:01 -0700

    Configure sumneko lua-language-server.

--- private_dot_config/nvim/lua/configs/lsp.lua
+++ private_dot_config/nvim/lua/configs/lsp.lua
@@ -3,30 +3,32 @@ local nvim_lsp = require('lspconfig')
 local function set_commands()
   -- Commands.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.cmd("command! LspDeclaration lua vim.lsp.buf.declaration()")
-  vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
-  vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-  vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
-  vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-  vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
-  vim.cmd("command! LspOrganize lua lsp_organize_imports()")
-  vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-  vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
-  vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
-  vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
-  vim.cmd("command! LspDiagShow lua vim.lsp.diagnostic.show_line_diagnostics()")
-  vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
-  vim.cmd("command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()")
-  vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
-  vim.cmd("command! LspWorkspaceList lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))")
-  vim.cmd("command! LspWorkspaceAdd lua vim.lsp.buf.add_workspace_folder()")
-  vim.cmd("command! LspWorkspaceRemove lua vim.lsp.buf.remove_workspace_folder()")
+  vim.cmd('command! LspDeclaration lua vim.lsp.buf.declaration()')
+  vim.cmd('command! LspDef lua vim.lsp.buf.definition()')
+  vim.cmd('command! LspFormatting lua vim.lsp.buf.formatting()')
+  vim.cmd('command! LspCodeAction lua vim.lsp.buf.code_action()')
+  vim.cmd('command! LspHover lua vim.lsp.buf.hover()')
+  vim.cmd('command! LspRename lua vim.lsp.buf.rename()')
+  vim.cmd('command! LspOrganize lua lsp_organize_imports()')
+  vim.cmd('command! LspRefs lua vim.lsp.buf.references()')
+  vim.cmd('command! LspTypeDef lua vim.lsp.buf.type_definition()')
+  vim.cmd('command! LspImplementation lua vim.lsp.buf.implementation()')
+  vim.cmd('command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()')
+  vim.cmd('command! LspDiagShow lua vim.lsp.diagnostic.show_line_diagnostics()')
+  vim.cmd('command! LspDiagNext lua vim.lsp.diagnostic.goto_next()')
+  vim.cmd('command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()')
+  vim.cmd('command! LspSignatureHelp lua vim.lsp.buf.signature_help()')
+  vim.cmd('command! LspWorkspaceList lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))')
+  vim.cmd('command! LspWorkspaceAdd lua vim.lsp.buf.add_workspace_folder()')
+  vim.cmd('command! LspWorkspaceRemove lua vim.lsp.buf.remove_workspace_folder()')
 end
 
-local function set_keymaps (bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+local function set_keymaps(bufnr)
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
   -- Mappings.
-  local opts = { noremap=true, silent=true }
+  local opts = { noremap = true, silent = true }
   buf_set_keymap('n', 'gD', '<Cmd>LspDeclaration<CR>', opts)
   buf_set_keymap('n', 'K', '<Cmd>LspHover<CR>', opts)
   buf_set_keymap('n', '<C-k>', '<cmd>LspSignatureHelp<CR>', opts)
@@ -54,7 +56,7 @@ end
 -- after the language server attaches to the current buffer
 local function on_attach(client, bufnr)
   if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+    vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
   end
   if vim.b.lsp_buffer_set_up then
     return
@@ -63,7 +65,9 @@ local function on_attach(client, bufnr)
   vim.b.lsp_buffer_set_up = 1
 
   --Enable completion triggered by <c-x><c-o>
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   set_commands()
@@ -71,46 +75,48 @@ local function on_attach(client, bufnr)
   require('illuminate').on_attach(client)
 end
 
-local function on_attach_no_format (client, bufnr)
+local function on_attach_no_format(client, bufnr)
   client.resolved_capabilities.document_formatting = false
   client.resolved_capabilities.document_range_formatting = false
   on_attach(client, bufnr)
 end
 
 local function set_signs()
-  local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+  local signs = { Error = ' ', Warning = ' ', Hint = ' ', Information = ' ' }
   for type, icon in pairs(signs) do
-    local hl = "LspDiagnosticsSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    local hl = 'LspDiagnosticsSign' .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
   end
 end
 
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
 
 local function setup()
-  nvim_lsp.pyright.setup {
+  nvim_lsp.pyright.setup({
     on_attach = on_attach_no_format,
-  }
+  })
 
-  nvim_lsp.tsserver.setup {
+  nvim_lsp.tsserver.setup({
     on_attach = function(client, bufnr)
-      local ts_utils = require("nvim-lsp-ts-utils")
-      ts_utils.setup {
-        enable_formatting = true
-      }
+      local ts_utils = require('nvim-lsp-ts-utils')
+      ts_utils.setup({
+        enable_formatting = true,
+      })
       on_attach_no_format(client, bufnr)
       ts_utils.setup_client(client)
-    end
-  }
+    end,
+  })
 
-  nvim_lsp.vimls.setup {
+  nvim_lsp.vimls.setup({
     on_attach = on_attach,
-  }
+  })
 
-
-  nvim_lsp.gopls.setup {
-    cmd = { "gopls", "-remote=auto" },
+  nvim_lsp.gopls.setup({
+    cmd = { 'gopls', '-remote=auto' },
     on_attach = on_attach,
-  }
+  })
 
   set_signs()
 end
--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -1,20 +1,20 @@
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'Shougo/denite.nvim'
-  use {'gotgenes/fruzzy', branch = 'increase-limit'}
-  use 'Shougo/neoyank.vim'
+  use('wbthomason/packer.nvim')
+  use('Shougo/denite.nvim')
+  use({ 'gotgenes/fruzzy', branch = 'increase-limit' })
+  use('Shougo/neoyank.vim')
 
-  use {
+  use({
     'hrsh7th/nvim-cmp',
     requires = {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lua',
-      'hrsh7th/cmp-path'
+      'hrsh7th/cmp-path',
     },
     config = function()
       local cmp = require('cmp')
-      cmp.setup {
+      cmp.setup({
         mapping = {
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -29,76 +29,70 @@ return require('packer').startup(function(use)
           { name = 'nvim_lua' },
           { name = 'path' },
         },
-      }
-    end
-  }
-  use 'machakann/vim-sandwich'
-  use 'schickling/vim-bufonly'
-  use 'Shougo/defx.nvim'
-  use 'kristijanhusak/defx-icons'
-  use 'kristijanhusak/defx-git'
-  use 'ntpeters/vim-better-whitespace'
-  use 'dbakker/vim-projectroot'
-  use 'vim-scripts/utl.vim'
-  use 'simnalamburt/vim-mundo'
-  use 'itchyny/lightline.vim'
-  use 'RRethy/nvim-base16'
-  use 'mike-hearn/base16-vim-lightline'
-  use 'zhou13/vim-easyescape'
-  use 'AndrewRadev/splitjoin.vim'
-  use 'embear/vim-localvimrc'
-  use {
+      })
+    end,
+  })
+  use('machakann/vim-sandwich')
+  use('schickling/vim-bufonly')
+  use('Shougo/defx.nvim')
+  use('kristijanhusak/defx-icons')
+  use('kristijanhusak/defx-git')
+  use('ntpeters/vim-better-whitespace')
+  use('dbakker/vim-projectroot')
+  use('vim-scripts/utl.vim')
+  use('simnalamburt/vim-mundo')
+  use('itchyny/lightline.vim')
+  use('RRethy/nvim-base16')
+  use('mike-hearn/base16-vim-lightline')
+  use('zhou13/vim-easyescape')
+  use('AndrewRadev/splitjoin.vim')
+  use('embear/vim-localvimrc')
+  use({
     'norcalli/nvim-colorizer.lua',
     config = function()
-      require 'colorizer'.setup(
-        {
-          '*',
-          '!packer',
-        },
-        {
-          names = false,
-        }
-      )
-    end
-  }
+      require('colorizer').setup({
+        '*',
+        '!packer',
+      }, {
+        names = false,
+      })
+    end,
+  })
 
   -- Git
-  use 'tpope/vim-fugitive'
-  use 'gregsexton/gitv'
-  use 'knsh14/vim-github-link'
-  use {
+  use('tpope/vim-fugitive')
+  use('gregsexton/gitv')
+  use('knsh14/vim-github-link')
+  use({
     'lewis6991/gitsigns.nvim',
     requires = {
-      'nvim-lua/plenary.nvim'
+      'nvim-lua/plenary.nvim',
     },
     config = function()
       require('gitsigns').setup()
-    end
-  }
+    end,
+  })
 
   -- CI
-  use 'martinda/Jenkinsfile-vim-syntax'
+  use('martinda/Jenkinsfile-vim-syntax')
 
   -- Programming
-  use {
+  use({
     'b3nj5m1n/kommentary',
     config = function()
-      require('kommentary.config').configure_language(
-        'default',
-        { prefer_single_line_comments = true }
-      )
-    end
-  }
-  use 'majutsushi/tagbar'
-  use {
+      require('kommentary.config').configure_language('default', { prefer_single_line_comments = true })
+    end,
+  })
+  use('majutsushi/tagbar')
+  use({
     'windwp/nvim-autopairs',
     after = 'nvim-cmp',
     config = function()
       require('configs.nvim-autopairs')
-    end
-  }
-  use 'Konfekt/FastFold'
-  use {
+    end,
+  })
+  use('Konfekt/FastFold')
+  use({
     'jose-elias-alvarez/null-ls.nvim',
     requires = {
       'nvim-lua/plenary.nvim',
@@ -106,21 +100,39 @@ return require('packer').startup(function(use)
     },
     config = function()
       require('configs.null-ls')
-      require('lspconfig')['null-ls'].setup {
-        on_attach = require('configs.lsp').on_attach
-      }
-    end
-  }
-  use 'RRethy/vim-illuminate'
-  use {
+      require('lspconfig')['null-ls'].setup({
+        on_attach = require('configs.lsp').on_attach,
+      })
+    end,
+  })
+  use('RRethy/vim-illuminate')
+  use({
+    'folke/lua-dev.nvim',
+    requires = 'neovim/nvim-lspconfig',
+    config = function()
+      local sumneko_lua_root = vim.fn.stdpath('data') .. '/lua-language-server/'
+      local luadev = require('lua-dev').setup({
+        lspconfig = {
+          cmd = {
+            sumneko_lua_root .. 'bin/macOS/lua-language-server',
+            '-E',
+            sumneko_lua_root .. 'main.lua',
+          },
+          on_attach = require('configs.lsp').on_attach_no_format,
+        },
+      })
+      require('lspconfig')['sumneko_lua'].setup(luadev)
+    end,
+  })
+  use({
     'neovim/nvim-lspconfig',
     config = function()
       require('configs.lsp').setup()
-    end
-  }
-  use 'josa42/nvim-lightline-lsp'
-  use 'nvim-lua/popup.nvim'
-  use {
+    end,
+  })
+  use('josa42/nvim-lightline-lsp')
+  use('nvim-lua/popup.nvim')
+  use({
     'nvim-telescope/telescope.nvim',
     requires = {
       'nvim-lua/plenary.nvim',
@@ -128,75 +140,75 @@ return require('packer').startup(function(use)
     },
     config = function()
       require('configs.telescope')
-    end
-  }
-  use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
+    end,
+  })
+  use({
+    'folke/trouble.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
     config = function()
-      require("trouble").setup()
-    end
-  }
-  use 'folke/lsp-colors.nvim'
+      require('trouble').setup()
+    end,
+  })
+  use('folke/lsp-colors.nvim')
 
   -- Go
-  use 'fatih/vim-go'
-  use 'gotgenes/golang-template.vim'
+  use('fatih/vim-go')
+  use('gotgenes/golang-template.vim')
 
   -- HTML
-  use 'tmhedberg/matchit'
-  use 'othree/html5.vim'
-  use 'alvan/vim-closetag'
+  use('tmhedberg/matchit')
+  use('othree/html5.vim')
+  use('alvan/vim-closetag')
 
   -- CSS / SASS
-  use 'hail2u/vim-css3-syntax'
-  use 'cakebaker/scss-syntax.vim'
+  use('hail2u/vim-css3-syntax')
+  use('cakebaker/scss-syntax.vim')
 
   -- GraphQL
-  use 'jparise/vim-graphql'
+  use('jparise/vim-graphql')
 
   -- JavaScript
-  use 'pangloss/vim-javascript'
-  use 'maxmellon/vim-jsx-pretty'
+  use('pangloss/vim-javascript')
+  use('maxmellon/vim-jsx-pretty')
 
   -- LaTeX
-  use 'lervag/vimtex'
+  use('lervag/vimtex')
 
   -- Markdown
-  use 'godlygeek/tabular'
-  use 'plasticboy/vim-markdown'
-  use {
+  use('godlygeek/tabular')
+  use('plasticboy/vim-markdown')
+  use({
     'iamcco/markdown-preview.nvim',
-    ft = {'markdown', 'pandoc.markdown', 'rmd'},
-    run = 'cd app & yarn install'
-  }
+    ft = { 'markdown', 'pandoc.markdown', 'rmd' },
+    run = 'cd app & yarn install',
+  })
 
   -- Python
-  use 'tmhedberg/SimpylFold'
-  use 'hynek/vim-python-pep8-indent'
-  use 'vim-python/python-syntax'
+  use('tmhedberg/SimpylFold')
+  use('hynek/vim-python-pep8-indent')
+  use('vim-python/python-syntax')
 
   -- Terraform
-  use 'hashivim/vim-terraform'
+  use('hashivim/vim-terraform')
 
   -- TOML
-  use 'cespare/vim-toml'
+  use('cespare/vim-toml')
 
   -- TypeScript
-  use 'leafgarland/typescript-vim'
-  use 'peitalin/vim-jsx-typescript'
-  use {
+  use('leafgarland/typescript-vim')
+  use('peitalin/vim-jsx-typescript')
+  use({
     'jose-elias-alvarez/nvim-lsp-ts-utils',
     requires = {
       'neovim/nvim-lspconfig',
       'jose-elias-alvarez/null-ls.nvim',
-      'nvim-lua/plenary.nvim'
-    }
-  }
+      'nvim-lua/plenary.nvim',
+    },
+  })
 
   -- Velocity
-  use 'lepture/vim-velocity'
+  use('lepture/vim-velocity')
 
   -- xonsh
-  use 'meatballs/vim-xonsh'
+  use('meatballs/vim-xonsh')
 end)
