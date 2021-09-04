56c466f30356a371025aa708579cd0c1843427ff
Chris Lasher <chris.lasher@gmail.com>
2021-09-03 20:19:34 -0700

    Move nvim-cmp config to its own file.

---
+++ private_dot_config/nvim/lua/configs/nvim-cmp.lua
@@ -0,0 +1,17 @@
+local cmp = require('cmp')
+cmp.setup({
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+  },
+  sources = {
+    { name = 'buffer' },
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'path' },
+  },
+})
--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -13,23 +13,7 @@ return require('packer').startup(function(use)
       'hrsh7th/cmp-path',
     },
     config = function()
-      local cmp = require('cmp')
-      cmp.setup({
-        mapping = {
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
-        },
-        sources = {
-          { name = 'buffer' },
-          { name = 'nvim_lsp' },
-          { name = 'nvim_lua' },
-          { name = 'path' },
-        },
-      })
+      require('configs.nvim-cmp')
     end,
   })
   use('machakann/vim-sandwich')
