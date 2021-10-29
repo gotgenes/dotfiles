f8c88b6e4631c4863874d3d47b703b692c673ef1
Chris Lasher <chris.lasher@gmail.com>
2021-10-29 09:33:32 -0700

    Change luasnip next binding to i_CTRL-L.
    i_CTRL-K is used for inserting digraphs, which can come in handy.

--- private_dot_config/nvim/lua/configs/luasnip.lua
+++ private_dot_config/nvim/lua/configs/luasnip.lua
@@ -6,8 +6,8 @@ luasnip.config.setup({
 local opts = {}
 vim.api.nvim_set_keymap('i', '<C-s>', '<Plug>luasnip-expand-snippet', opts)
 vim.api.nvim_set_keymap('i', '<C-j>', '<Plug>luasnip-jump-next', opts)
-vim.api.nvim_set_keymap('i', '<C-k>', '<Plug>luasnip-jump-prev', opts)
+vim.api.nvim_set_keymap('i', '<C-l>', '<Plug>luasnip-jump-prev', opts)
 
 vim.api.nvim_set_keymap('s', '<C-s>', '<Plug>luasnip-expand-snippet', opts)
 vim.api.nvim_set_keymap('s', '<C-j>', '<Plug>luasnip-jump-next', opts)
-vim.api.nvim_set_keymap('s', '<C-k>', '<Plug>luasnip-jump-prev', opts)
+vim.api.nvim_set_keymap('s', '<C-l>', '<Plug>luasnip-jump-prev', opts)
