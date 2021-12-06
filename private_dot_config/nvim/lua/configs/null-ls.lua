0bb81a1db75448135551eeede363fa94ec9d1d3c
Chris Lasher <chris.lasher@gmail.com>
2021-12-06 11:29:54 -0800

    Add linting and formatting for TypeScript and JavaScript.

--- private_dot_config/nvim/lua/configs/null-ls.lua
+++ private_dot_config/nvim/lua/configs/null-ls.lua
@@ -1,6 +1,9 @@
 local null_ls = require('null-ls')
+local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
+
+local node_local = { prefer_local = 'node_modules/.bin' }
 
 null_ls.config({
   sources = {
@@ -8,5 +11,8 @@ null_ls.config({
     diagnostics.flake8,
     formatting.black,
     formatting.stylua,
+    diagnostics.eslint_d.with(node_local),
+    code_actions.eslint_d.with(node_local),
+    null_ls.builtins.formatting.prettierd.with(node_local),
   },
 })
