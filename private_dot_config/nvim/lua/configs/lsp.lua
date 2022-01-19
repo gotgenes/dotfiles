b496b3d637756c873b1f84627cd7d03f812391b0
Chris Lasher <chris.lasher@gmail.com>
2022-01-18 16:34:03 -0800

    Fix diagnostic gutter signs.

--- private_dot_config/nvim/lua/configs/lsp.lua
+++ private_dot_config/nvim/lua/configs/lsp.lua
@@ -86,9 +86,9 @@ local function on_attach_no_format(client, bufnr)
 end
 
 local function set_signs()
-  local signs = { Error = ' ', Warning = ' ', Hint = ' ', Information = ' ' }
+  local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
   for type, icon in pairs(signs) do
-    local hl = 'LspDiagnosticsSign' .. type
+    local hl = 'DiagnosticSign' .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
   end
 end
