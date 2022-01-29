5dc2a918bdaa78eb5995a5960c5cbfc4abdeda25
Chris Lasher <chris.lasher@gmail.com>
2022-01-28 22:29:06 -0800

    Update the path to lua language server.

--- private_dot_config/nvim/lua/configs/lua-dev.lua
+++ private_dot_config/nvim/lua/configs/lua-dev.lua
@@ -2,7 +2,7 @@ local sumneko_lua_root = vim.fn.stdpath('data') .. '/lua-language-server/'
 local luadev = require('lua-dev').setup({
   lspconfig = {
     cmd = {
-      sumneko_lua_root .. 'bin/macOS/lua-language-server',
+      sumneko_lua_root .. 'bin/lua-language-server',
       '-E',
       sumneko_lua_root .. 'main.lua',
     },
