7da4428c74e66d201fb257be20aae8e17f4ec4ff
Chris Lasher <chris.lasher@gmail.com>
2022-05-13 22:07:21 -0700

    Add dressing.nvim.

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -89,6 +89,12 @@ return require('packer').startup(function(use)
     end,
   })
   use({
+    'stevearc/dressing.nvim',
+    requires = {
+      'nvim-telescope/telescope.nvim',
+    },
+  })
+  use({
     'kyazdani42/nvim-tree.lua',
     requires = {
       'kyazdani42/nvim-web-devicons', -- optional, for file icon
