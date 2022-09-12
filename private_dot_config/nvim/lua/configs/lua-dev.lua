975c2ff7bf64528a0c31e49b26192bda60f17575
Chris Lasher <chris.lasher@gmail.com>
2022-09-11 19:22:00 -0700

    Use simplified lua-dev setup.

--- private_dot_config/nvim/lua/configs/lua-dev.lua
+++ private_dot_config/nvim/lua/configs/lua-dev.lua
@@ -5,17 +5,11 @@ local nvim_lsp = require('lspconfig')
 local lsp_config = require('configs.lsp')
 
 function M.setup()
-  local runtime_path = vim.split(package.path, ';')
-  table.insert(runtime_path, 'lua/?.lua')
-  table.insert(runtime_path, 'lua/?/init.lua')
-
-  local luadev = lua_dev.setup({
-    lspconfig = {
-      capabilities = lsp_config.capabilities,
-      on_attach = lsp_config.on_attach,
-    },
+  lua_dev.setup()
+  nvim_lsp.sumneko_lua.setup({
+    capabilities = lsp_config.capabilities,
+    on_attach = lsp_config.on_attach,
   })
-  nvim_lsp.sumneko_lua.setup(luadev)
 end
 
 return M
