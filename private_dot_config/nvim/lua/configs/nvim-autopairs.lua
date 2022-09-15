6641fdc84f2adcc027abee66792fd87861b36431
Chris Lasher <chris.lasher@gmail.com>
2022-09-15 11:41:51 -0700

    Use shorter rules for autopairs.

--- private_dot_config/nvim/lua/configs/nvim-autopairs.lua
+++ private_dot_config/nvim/lua/configs/nvim-autopairs.lua
@@ -13,41 +13,33 @@ function M.setup()
   cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
   npairs.add_rules({
-    Rule(' ', ' ')
-      :with_pair(function(opts)
-        local pair = opts.line:sub(opts.col - 1, opts.col)
-        return vim.tbl_contains({ '()', '{}', '[]' }, pair)
+    Rule(' ', ' '):with_pair(function(opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+    Rule('( ', ' )')
+      :with_pair(function()
+        return false
       end)
-      :with_move(cond.none())
-      :with_cr(cond.none())
-      :with_del(function(opts)
-        local col = vim.api.nvim_win_get_cursor(0)[2]
-        local context = opts.line:sub(col - 1, col + 2)
-        return vim.tbl_contains({ '(  )', '{  }', '[  ]' }, context)
-      end),
-    Rule('', ' )')
-      :with_pair(cond.none())
       :with_move(function(opts)
-        return opts.char == ')'
+        return opts.prev_char:match('.%)') ~= nil
       end)
-      :with_cr(cond.none())
-      :with_del(cond.none())
       :use_key(')'),
-    Rule('', ' }')
-      :with_pair(cond.none())
-      :with_move(function(opts)
-        return opts.char == '}'
+    Rule('{ ', ' }')
+      :with_pair(function()
+        return false
       end)
-      :with_cr(cond.none())
-      :with_del(cond.none())
+      :with_move(function(opts)
+        return opts.prev_char:match('.%}') ~= nil
+      end)
       :use_key('}'),
-    Rule('', ' ]')
-      :with_pair(cond.none())
-      :with_move(function(opts)
-        return opts.char == ']'
+    Rule('[ ', ' ]')
+      :with_pair(function()
+        return false
       end)
-      :with_cr(cond.none())
-      :with_del(cond.none())
+      :with_move(function(opts)
+        return opts.prev_char:match('.%]') ~= nil
+      end)
       :use_key(']'),
   })
 end
