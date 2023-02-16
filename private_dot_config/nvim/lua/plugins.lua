8a5c13414a9a8e13798db20b261c5332351cf95f
Chris Lasher <chris.lasher@gmail.com>
2023-02-16 10:20:25 -0800

    Add cmp-cmdline dependency.

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -28,6 +28,7 @@ local spec = {
     'hrsh7th/nvim-cmp',
     dependencies = {
       'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lua',
       'hrsh7th/cmp-path',
