96aed54da4063a9cf1a70e1afdb03147afe96095
Chris Lasher <chris.lasher@gmail.com>
2023-04-01 20:38:00 -0700

    Set dap-python keybindings on filetype load.

---
+++ private_dot_config/nvim/after/ftplugin/python.lua
@@ -0,0 +1,2 @@
+local buf = vim.api.nvim_get_current_buf()
+require('configs.plugins.dap-python').set_keymaps(buf)
--- private_dot_config/nvim/lua/configs/plugins/dap-python.lua
+++ private_dot_config/nvim/lua/configs/plugins/dap-python.lua
@@ -1,9 +1,8 @@
 local M = {}
 
-local dap_python = require('dap-python')
-local wk = require('which-key')
+function M.set_keymaps(bufnr)
+  local wk = require('which-key')
 
-local function set_keymaps(bufnr)
   wk.register({
     d = {
       t = {
@@ -19,12 +18,10 @@ local function set_keymaps(bufnr)
 end
 
 function M.setup()
+  local dap_python = require('dap-python')
+
   dap_python.setup()
   dap_python.test_runner = 'pytest'
-  _G.set_dap_python_keybindings = function()
-    local buf = vim.api.nvim_get_current_buf()
-    set_keymaps(buf)
-  end
 end
 
 return M
