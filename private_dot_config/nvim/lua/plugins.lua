f82ea73e156c6b35babf88c2029a078579245104
Chris Lasher <chris.lasher@gmail.com>
2023-06-03 13:15:24 -0700

    Use lazy.vim spec structure.
    See https://github.com/folke/lazy.nvim/tree/main#-structuring-your-plugins

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -1,6 +1,4 @@
-local M = {}
-
-local spec = {
+return {
   {
     'catppuccin/nvim',
     name = 'catppuccin',
@@ -512,35 +510,3 @@ local spec = {
   -- xonsh
   { 'meatballs/vim-xonsh' },
 }
-
-local function ensure_lazy_installed()
-  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable', -- latest stable release
-      lazypath,
-    })
-  end
-  if not vim.tbl_contains(vim.opt.rtp:get(), lazypath) then
-    vim.opt.rtp:prepend(lazypath)
-  end
-end
-
-local function configure_lazy_keymapping()
-  local ViewConfig = require('lazy.view.config')
-  ViewConfig.keys.profile_filter = '<C-p>'
-end
-
-function M.setup()
-  vim.g.no_python_maps = 1
-
-  ensure_lazy_installed()
-  configure_lazy_keymapping()
-  require('lazy').setup(spec)
-end
-
-return M
