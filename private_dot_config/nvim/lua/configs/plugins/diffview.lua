2254e88e36f7a5fd2e16191569ad5db3d88d29e2
Chris Lasher <chris.lasher@gmail.com>
2023-06-03 12:09:46 -0700

    Add diffview hooks to help with nvim-ufo.
    See https://github.com/kevinhwang91/nvim-ufo/issues/57#issuecomment-1520854713

--- private_dot_config/nvim/lua/configs/plugins/diffview.lua
+++ private_dot_config/nvim/lua/configs/plugins/diffview.lua
@@ -14,6 +14,17 @@ function M.setup()
         layout = 'diff2_vertical',
       },
     },
+    enhanced_diff_hl = true,
+    hooks = {
+      diff_buf_read = function()
+        vim.opt_local.foldlevel = 99
+        vim.opt_local.foldenable = false
+      end,
+      diff_buf_win_enter = function()
+        vim.opt_local.foldlevel = 99
+        vim.opt_local.foldenable = false
+      end,
+    },
   })
 end
 
