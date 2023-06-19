ea4cc7805700f08eb372948b505bd4f80ad04aef
Chris Lasher <chris.lasher@gmail.com>
2023-06-18 22:38:23 -0700

    Use neodev defaults.

--- private_dot_config/nvim/lua/configs/plugins/neodev.lua
+++ private_dot_config/nvim/lua/configs/plugins/neodev.lua
@@ -4,7 +4,7 @@ function M.setup()
   local neodev = require('neodev')
   local nvim_lsp = require('lspconfig')
   local lsp_config = require('configs.plugins.lsp')
-  neodev.setup({ library = { plugins = { 'nvim-dap-ui' }, types = true } })
+  neodev.setup({})
   nvim_lsp.lua_ls.setup({
     capabilities = lsp_config.capabilities,
     on_attach = lsp_config.on_attach,
