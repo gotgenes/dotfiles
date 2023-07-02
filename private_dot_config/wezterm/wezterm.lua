a06f4c5e4ee623b38deaa43d81c410d7c445fe50
Chris Lasher <chris.lasher@gmail.com>
2023-07-02 09:07:59 -0700

    Maximize window on startup.

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,4 +1,15 @@
 local wezterm = require('wezterm')
+local mux = wezterm.mux
+
+wezterm.on('gui-attached', function(domain)
+  -- maximize all displayed windows on startup
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
+end)
 
 local config = wezterm.config_builder()
 
@@ -6,7 +17,5 @@ config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular' })
 config.font_size = 12.0
 config.line_height = 1.05
 config.color_scheme = 'Catppuccin Macchiato'
-config.initial_rows = 60
-config.initial_cols = 180
 
 return config
