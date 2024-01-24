b42efa133e8f54ce717114dd864b1a0c9bbc0741
Chris Lasher <chris.lasher@gmail.com>
2024-01-23 22:08:47 -0500

    Explicitly define nvim-cmp mappings.

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -5,10 +5,29 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      opts.mapping = {
+        ["<Down>"] = {
+          i = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        },
+        ["<Up>"] = {
+          i = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        },
         ["<Tab>"] = cmp.mapping.select_next_item(),
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-      })
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-CR>"] = function(fallback)
+          cmp.abort()
+          fallback()
+        end,
+      }
     end,
   },
   {
