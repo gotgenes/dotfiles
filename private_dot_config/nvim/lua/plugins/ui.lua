c7b077e8b3d1788d07fb2b27185aa264dfd329ed
Chris Lasher <chris.lasher@gmail.com>
2024-01-29 10:43:53 -0500

    Customize Dashboard.

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -68,6 +68,68 @@ return {
     },
   },
   {
+    "nvimdev/dashboard-nvim",
+    opts = function(_, opts)
+      opts.config.center = {
+
+        {
+          action = "Telescope smart_open cwd_only=true",
+          desc = " Find file",
+          icon = " ",
+          key = "f",
+        },
+        {
+          action = "ene | startinsert",
+          desc = " New file",
+          icon = " ",
+          key = "n",
+        },
+        {
+          action = [[lua require("telescope.builtin").oldfiles({ only_cwd = true })]],
+          desc = " Recent files",
+          icon = " ",
+          key = "r",
+        },
+        {
+          action = "Telescope live_grep",
+          desc = " Find text",
+          icon = " ",
+          key = "g",
+        },
+        {
+          action = [[lua require("lazyvim.util").telescope.config_files()()]],
+          desc = " Config",
+          icon = " ",
+          key = "c",
+        },
+        {
+          action = 'lua require("persistence").load()',
+          desc = " Restore Session",
+          icon = " ",
+          key = "s",
+        },
+        {
+          action = "LazyExtras",
+          desc = " Lazy Extras",
+          icon = " ",
+          key = "x",
+        },
+        {
+          action = "Lazy",
+          desc = " Lazy",
+          icon = "󰒲 ",
+          key = "l",
+        },
+        {
+          action = "qa",
+          desc = " Quit",
+          icon = " ",
+          key = "q",
+        },
+      }
+    end,
+  },
+  {
     "uga-rosa/ccc.nvim",
     opts = {},
     cmd = {
