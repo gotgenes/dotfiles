84c1604d77eb1d703b91db5404374251552e3f92
Chris Lasher <chris.lasher@gmail.com>
2024-04-09 14:41:49 -0400

    Move diffview configuration.

--- private_dot_config/nvim/lua/plugins/programming.lua
+++ private_dot_config/nvim/lua/plugins/programming.lua
@@ -218,9 +218,39 @@ return {
   {
     "sindrets/diffview.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("config.plugins.diffview")
-    end,
+    command = {
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewLog",
+      "DiffviewOpen",
+      "DiffviewRefresh",
+      "DiffviewToggleFiles",
+    },
+    opts = {
+      view = {
+        default = {
+          layout = "diff2_vertical",
+        },
+        merge_tool = {
+          layout = "diff3_vertical",
+        },
+        file_history = {
+          layout = "diff2_vertical",
+        },
+      },
+      enhanced_diff_hl = true,
+      hooks = {
+        diff_buf_read = function()
+          vim.opt_local.foldlevel = 99
+          vim.opt_local.foldenable = false
+        end,
+        diff_buf_win_enter = function()
+          vim.opt_local.foldlevel = 99
+          vim.opt_local.foldenable = false
+        end,
+      },
+    },
   },
   {
     "zbirenbaum/copilot.lua",
