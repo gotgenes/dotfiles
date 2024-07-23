d94b9b2fb03472d02ce51a7b1c20f6afbee0fcc0
Chris Lasher <chris.lasher@gmail.com>
2024-07-22 20:58:04 -0400

    Disable inlay hints by default.

--- private_dot_config/nvim/lua/plugins/programming.lua
+++ private_dot_config/nvim/lua/plugins/programming.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
