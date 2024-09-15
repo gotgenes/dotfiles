0cf0d6211efddd33c58e78dbd2b10c0a993ac28c
Chris Lasher <chris.lasher@gmail.com>
2024-09-15 15:54:32 -0400

    Remove unused LSP helper.

--- private_dot_config/nvim/lua/helpers.lua
+++ private_dot_config/nvim/lua/helpers.lua
@@ -1,16 +1,5 @@
 local M = {}
 
----@param on_attach fun(client, buffer)
-function M.on_lsp_attach(on_attach)
-  vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-      local buffer = args.buf
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      on_attach(client, buffer)
-    end,
-  })
-end
-
 -- Color conversions functions originally found in:
 -- https://github.com/farias-hecdin/CSSVarHighlight/blob/main/lua/CSSVarHighlight/convert_color.lua
 
