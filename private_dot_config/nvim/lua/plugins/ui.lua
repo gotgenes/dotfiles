c0c2976002c25b514b88990dac42b011f729add6
Chris Lasher <chris.lasher@gmail.com>
2024-11-25 13:08:03 -0500

    Switch from Dashboard to Snacks.

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -163,73 +163,75 @@ return {
     opts = require("config.plugins.lualine").opts,
   },
   {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      opts.config.center = {
-        {
-          action = "Telescope smart_open cwd_only=true",
-          desc = " Find file",
-          icon = " ",
-          key = "f",
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          keys = {
+            {
+              action = ":Telescope smart_open cwd_only=true",
+              desc = " Find file",
+              icon = " ",
+              key = "f",
+            },
+            {
+              action = ":ene | startinsert",
+              desc = " New file",
+              icon = " ",
+              key = "n",
+            },
+            {
+              action = ":lua Snacks.dashboard.pick('oldfiles', { only_cwd = true })",
+              desc = " Recent files",
+              icon = " ",
+              key = "r",
+            },
+            {
+              action = ":lua Snacks.dashboard.pick('live_grep')",
+              desc = " Find text",
+              icon = " ",
+              key = "g",
+            },
+            {
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+              desc = " Config",
+              icon = " ",
+              key = "c",
+            },
+            {
+              desc = " Restore Session",
+              section = "session",
+              icon = " ",
+              key = "s",
+            },
+            {
+              action = ":LazyExtras",
+              desc = " Lazy Extras",
+              icon = " ",
+              key = "x",
+            },
+            {
+              action = ":Lazy",
+              desc = " Lazy",
+              icon = "󰒲 ",
+              key = "l",
+            },
+            {
+              action = ":Mason",
+              desc = " Mason",
+              icon = "󱌢 ",
+              key = "m",
+            },
+            {
+              action = ":qa",
+              desc = " Quit",
+              icon = " ",
+              key = "q",
+            },
+          },
         },
-        {
-          action = "ene | startinsert",
-          desc = " New file",
-          icon = " ",
-          key = "n",
-        },
-        {
-          action = [[lua LazyVim.pick("oldfiles")({ only_cwd = true })]],
-          desc = " Recent files",
-          icon = " ",
-          key = "r",
-        },
-        {
-          action = [[lua LazyVim.pick("live_grep")()]],
-          desc = " Find text",
-          icon = " ",
-          key = "g",
-        },
-        {
-          action = [[lua LazyVim.pick.config_files()()]],
-          desc = " Config",
-          icon = " ",
-          key = "c",
-        },
-        {
-          action = 'lua require("persistence").load()',
-          desc = " Restore Session",
-          icon = " ",
-          key = "s",
-        },
-        {
-          action = "LazyExtras",
-          desc = " Lazy Extras",
-          icon = " ",
-          key = "x",
-        },
-        {
-          action = "Lazy",
-          desc = " Lazy",
-          icon = "󰒲 ",
-          key = "l",
-        },
-        {
-          action = "Mason",
-          desc = " Mason",
-          icon = "󱌢 ",
-          key = "m",
-        },
-        {
-          action = function()
-            vim.api.nvim_input("<cmd>qa<cr>")
-          end,
-          desc = " Quit",
-          icon = " ",
-          key = "q",
-        },
-      }
-    end,
+      },
+    },
   },
   {
     "uga-rosa/ccc.nvim",
