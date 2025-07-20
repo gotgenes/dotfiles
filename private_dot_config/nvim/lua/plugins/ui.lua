a9de9e4acba1b1756190ab36f63039eb0c659609
Chris Lasher <chris.lasher@gmail.com>
2025-07-20 14:28:48 -0400

    Switch to snacks.explorer.

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -164,6 +164,7 @@ return {
   },
   {
     "folke/snacks.nvim",
+    ---@type snacks.Config
     opts = {
       dashboard = {
         preset = {
@@ -232,7 +233,51 @@ return {
         },
       },
       picker = {
-        sources = { buffers = { layout = "select" } },
+        actions = {
+          copy_selector = function(_, item)
+            local modify = vim.fn.fnamemodify
+            local filepath = Snacks.picker.util.path(item)
+            if not filepath then
+              Snacks.notify("No file selected", vim.log.levels.WARN)
+              return
+            end
+            Snacks.notify(("Selected: `%s`"):format(filepath))
+            local filename = modify(filepath, ":t")
+
+            local vals = {
+              ["FILENAME"] = filename,
+              ["PATH (CWD)"] = modify(filepath, ":."),
+              ["BASENAME"] = modify(filename, ":r"),
+              ["FULL PATH"] = filepath,
+              ["PATH (HOME)"] = modify(filepath, ":~"),
+              ["URI"] = vim.uri_from_fname(filepath),
+            }
+
+            local options = vim.tbl_filter(function(val)
+              return vals[val] ~= ""
+            end, vim.tbl_keys(vals))
+            if vim.tbl_isempty(options) then
+              vim.notify("No values to copy", vim.log.levels.WARN)
+              return
+            end
+            Snacks.picker.select(options, {
+              prompt = "Choose to copy to clipboard:",
+              format_item = function(f_item)
+                return ("%s: %s"):format(f_item, vals[f_item])
+              end,
+            }, function(choice)
+              local result = vals[choice]
+              if result then
+                Snacks.notify(("Copied: `%s`"):format(result))
+                vim.fn.setreg("+", result)
+              end
+            end)
+          end,
+        },
+        sources = {
+          buffers = { layout = "select" },
+          explorer = { win = { list = { keys = { Y = "copy_selector" } } } },
+        },
       },
       scroll = {
         animate = {
