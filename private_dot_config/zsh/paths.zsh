8980e3cf6f31eefbf651295e90437e3302fa337e
Chris Lasher <chris.lasher@gmail.com>
2026-03-03 20:35:23 -0500

    Simplify zsh PATH setup; activate mise directly instead of via evalcache.

--- private_dot_config/zsh/paths.zsh
+++ private_dot_config/zsh/paths.zsh
@@ -2,6 +2,8 @@ if [[ -d "$HOME/.docker/bin" ]]; then
     export path=("$HOME/.docker/bin" $path)
 fi
 
+# Re-prepend homebrew ahead of system paths. brew shellenv adds these in .zshenv,
+# but /etc/zprofile's path_helper demotes them behind system paths for login shells.
 if [[ -v HOMEBREW_PREFIX ]]; then
     export path=("$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin" $path)
 fi
