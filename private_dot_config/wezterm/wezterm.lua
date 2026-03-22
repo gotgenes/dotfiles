c79010a78ba28b18530d398fd52e318d863fe184
Chris Lasher <chris.lasher@gmail.com>
2026-03-22 17:30:37 -0400

    Swap WezTerm Option key behavior: left composes, right passes ALT.

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -81,6 +81,8 @@ local my_config = {
   line_height = 1.2,
   color_scheme = 'Catppuccin Macchiato',
   hide_tab_bar_if_only_one_tab = true,
+  send_composed_key_when_left_alt_is_pressed = true,
+  send_composed_key_when_right_alt_is_pressed = false,
   keys = {
     {
       key = 'o',
